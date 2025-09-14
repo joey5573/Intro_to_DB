d5309f98d583da4021358c3bf3bb59858e7b3ffd
joey5573 <Siff.fox@gmail.com>
2025-09-14 19:28:25 +0200

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,5 @@
+-- Show full description of books table without using DESCRIBE or EXPLAIN
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
