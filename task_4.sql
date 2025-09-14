273280ca76a56510aea35c86125b29e0a8dea14d
joey5573 <Siff.fox@gmail.com>
2025-09-14 19:35:15 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,9 +1,3 @@
-- -- Select the correct database
-USE alx_book_store;
+ USE alx_book_store;
 
--- Get full description of the table 'books' without DESCRIBE or EXPLAIN
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'Books';
-
+SHOW CREATE TABLE books;
